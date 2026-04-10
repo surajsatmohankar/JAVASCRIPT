0bfdf5ba944f28e3e265b3ca7e956bba9b1eaa42
suraj.satmohankar <suraj.satmohankar@gmail.com>
2026-04-10 13:15:30 +0530

    Javascript study code

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 124;
+let accountEmail = "suraj@google.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+//accountId =2;
+accountEmail = "HDFC@Gmail.com"
+accountPassword = "124567"
+accountCity = "Bengaluru"
+console.log(accountCity);
+console.table([accountEmail,accountId,accountCity,accountPassword
+]); 
