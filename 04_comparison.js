c52d1a87e280abf6908087cfdbd7a1a7e3c1fd00
suraj.satmohankar <suraj.satmohankar@gmail.com>
2026-04-10 15:35:52 +0530

    comparison type

---
+++ 04_comparison.js
@@ -0,0 +1,11 @@
+console.log(null >0)
+console.log(null == 0)
+console.log(null < 0)
+console.log(null <= 0);  // comparion works diffenctly converts it into 0
+console.log(null >= 0); // comparion works diffenctly converts it into 0
+
+
+
+console.log(undefined == 0)
+console.log(undefined >0)
+console.log(undefined < 0); 
