99e790960224d432cd16a02c36d86dcaf02ea5d2
suraj.satmohankar <suraj.satmohankar@gmail.com>
2026-04-10 15:56:30 +0530

    Data tye summery

---
+++ 05_DataTypes.js
@@ -0,0 +1,13 @@
+
+const heroes = ["shaktiman", "naagraj", "doga", "Kilvish"];
+
+let myObj = {
+    name : "hitesh",
+    age : 31,
+    hobbies : "Dance"
+}
+
+console.log(myObj);
+console.table([heroes,myObj]);
+
+console.log(typeof null);
