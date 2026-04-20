485aca586ad165c7fb81ba42488b57372fafbacd
suraj.satmohankar <suraj.satmohankar@gmail.com>
2026-04-20 17:02:22 +0530

    control_flow

---
+++ 002_Context _Flow/01_conditions.js
@@ -0,0 +1,33 @@
+let balance = 100000;
+
+if(balance < 20000){
+    console.log("please inrease your bank balance");
+    
+}else{
+    console.log("Thanks for maintaining your credit");
+}
+
+const month = 1
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+
+    case 2:
+        console.log("FEB");
+        
+        break;
+    case 3:
+        console.log("MAR");
+        
+        break;
+    case 4:
+        console.log("APRIL");
+        
+        break;
+        
+
+    default:
+        break;
+}
